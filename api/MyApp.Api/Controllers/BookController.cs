09906eb214ad437b68885ad63643b2a8c86c40df
lihl <lihl@1handcar.com>
2018-07-30 16:17:56 +0800

    1

---
+++ api/MyApp.Api/Controllers/BookController.cs
@@ -0,0 +1,48 @@
+﻿using MyApp.Data;
+using MyApp.Services.BookService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MyApp.Api.Controllers
+{
+    public class BookController : ApiController
+    {
+        private readonly BookService _bookService;
+        // GET: Order
+        public BookController()
+        {
+            _bookService = new BookService();
+            
+        }
+
+        [System.Web.Http.ActionName("GetBooks")]
+        [System.Web.Http.HttpGet]
+        public List<MyApp_Book> GetBooks() {
+            return _bookService.GetBooks();
+        }
+
+        [System.Web.Http.ActionName("GetHeroes")]
+        [System.Web.Http.HttpGet]
+        [AllowAnonymous]
+        public hero[] GetHeroes()
+        {
+            System.Web.HttpContext.Current.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            var a = new hero[1];
+            a[0]=new hero()
+            {
+                id = "11",
+                name = "ssss"
+
+            };
+            return a;
+        }
+
+        public class hero {
+            public string id { get; set; }
+            public string name { get; set; }
+        }
+    }
+}
---
+++ api/MyApp.Services/BookService/BookService.cs
@@ -0,0 +1,23 @@
+﻿using MyApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Services.BookService
+{
+    public class BookService : ServiceBase
+    {
+
+        public List<MyApp_Book> GetBooks() {
+            var res = new List<MyApp_Book>();
+            MyAppEntities dbcontext = new MyAppEntities();
+            res= dbcontext.MyApp_Book.AsNoTracking().ToList();
+
+            return res;
+        }
+
+    }
+}
